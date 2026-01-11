d32bee70b770f83ebad2ac3f1efefd1c2a0509e4
Tsutomu Takao <takao.swe@gmail.com>
2026-01-11 21:45:38 +0900

    distributed: add trace aggregator server
    Signed-off-by: Tsutomu Takao <takao.swe@gmail.com>

--- retis/src/core/distributed/mod.rs
+++ retis/src/core/distributed/mod.rs
@@ -1,3 +1,4 @@
+pub(crate) mod aggregator;
 pub(crate) mod client;
 #[allow(dead_code)]
 pub(crate) mod node_id;
@@ -5,6 +6,7 @@ pub(crate) mod node_id;
 pub(crate) mod ntp;
 pub(crate) mod protocol;
 
+pub(crate) use aggregator::{AggregatorConfig, EventSink, LoggingEventSink, TraceAggregator};
 pub(crate) use client::{DistributedCollector, DistributedCollectorConfig};
 pub(crate) use node_id::NodeIdentity;
 #[allow(unused_imports)]
